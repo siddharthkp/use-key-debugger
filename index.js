481a760eb9410c2fcfa20370824dbfa8a9a064a9
Bhaskar Gyan Vardhan <bhaskar@moonraft.com>
2019-01-31 19:26:43 +0530

    added support for multi key press

--- index.js
+++ index.js
@@ -7,49 +7,10 @@ const symbolKeys = {
   ArrowDown: '▼'
 }
 
-const useKeyDebugger = () => {
-  const [key, setKey] = useState(null)
-
-  function callback(event = {}) {
-    setKey(symbolKeys[event.key] || event.key)
-  }
-
-  function reset() {
-    setKey(null)
-  }
-
-  useEffect(
-    function() {
-      window.addEventListener('keydown', callback)
-      const timeout = window.setTimeout(reset, 500)
-
-      return function cleanup() {
-        window.clearTimeout(timeout)
-        return window.removeEventListener('keydown', callback)
-      }
-    },
-    [key]
-  )
-
-  return function(props) {
-    if (key)
-      return (
-        <div style={styles} {...props}>
-          {key}
-        </div>
-      )
-    else return null
-  }
-}
-
-export default useKeyDebugger
-
 const styles = {
-  position: 'fixed',
-  top: '30px',
-  right: '30px',
   minHeight: '50px',
   minWidth: '50px',
+  marginRight: '10px',
   padding: '5px',
   display: 'flex',
   justifyContent: 'center',
@@ -60,3 +21,71 @@ const styles = {
   fontSize: '28px',
   borderRadius: '5px'
 }
+const wrapperStyle = {
+  position: 'fixed',
+  top: '30px',
+  right: '30px',
+  display: 'flex'
+}
+const useKeyDebugger = () => {
+  const [key, setKey] = useState([])
+
+  function onKeyDown(event = {}) {
+    const tempkey = JSON.parse(JSON.stringify(key))
+    const currentKey = symbolKeys[event.key] || event.key
+    if (!event.repeat) {
+      tempkey.push(currentKey)
+      setKey(tempkey)
+    }
+  }
+
+  function onKeyUp(event = {}) {
+    let tempkey = JSON.parse(JSON.stringify(key))
+    const currentKey = symbolKeys[event.key] || event.key
+    tempkey = tempkey.filter(val => val !== currentKey)
+    setKey(tempkey)
+  }
+
+  function reset() {
+    setKey([])
+  }
+
+  useEffect(
+    function() {
+      window.addEventListener('keydown', onKeyDown)
+      const timeout = window.setTimeout(reset, 500)
+
+      return function cleanup() {
+        window.clearTimeout(timeout)
+        return window.removeEventListener('keydown', onKeyDown)
+      }
+    },
+    [key.length]
+  )
+
+  useEffect(
+    function() {
+      window.addEventListener('keyup', onKeyUp)
+      return function cleanup() {
+        return window.removeEventListener('keyup', onKeyUp)
+      }
+    },
+    [key.length]
+  )
+
+  return function(props) {
+    return key.length ? (
+      <div style={wrapperStyle}>
+        {key.map((k, index) => {
+          return (
+            <div style={styles} {...props} key={k}>
+              {k}
+            </div>
+          )
+        })}
+      </div>
+    ) : null
+  }
+}
+
+export default useKeyDebugger
